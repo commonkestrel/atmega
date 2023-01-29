7ddfc53d8f0baee5346bf5993923a0bbbc19b2d2
Jibble330 <jettb6@gmail.com>
2023-01-29 12:50:56 -0800

    Test millis

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,27 @@
 
 use atmega::prelude::*;
 
-run!(setup, runner);
+run!(setup, run);
 
 struct State {
-    original_millis: u32,
+    prev_millis: u32,
 }
 
 /// Called once.
 /// Used to initialize pins and peripherals.
 /// Equivalent to the `setup` function in the Arduino language.
 fn setup() -> State {
-    //pin_mode(Pin::D7, PinMode::INPUT_PULLUP);
-    //pin_mode(Pin::D8, PinMode::OUTPUT);
     pin_mode(Pin::D9, PinMode::OUTPUT);
-    digital_write(Pin::D9, HIGH);
     State {
-        original_millis: millis(),
+        prev_millis: millis(),
     }
 }
 
 /// Called in a loop indefinitly.
 /// Equivalent to the `loop` function in the Arduino language.
-fn runner(state: &mut State) {
-    //let button = !digital_read(Pin::D7);
-    //digital_write(Pin::D8, button);
-
-    //digital_toggle(Pin::D8);
-    //delay(1000);
-
-    if millis() > 10000 {
-        digital_write(Pin::D9, LOW);
+fn run(state: &mut State) {
+    if millis() - state.prev_millis > 1000 {
+        digital_toggle(Pin::D9);
+        state.prev_millis = millis();
     }
 }
