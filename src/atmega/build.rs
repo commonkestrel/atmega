e55b9a96a6ef2e5a0845af3e8e97e137203b6dce
Jibble330 <jettb6@gmail.com>
2023-02-05 22:42:47 -0800

    Added cargo warnings for invalid env variables

--- src/atmega/build.rs
+++ src/atmega/build.rs
@@ -2,23 +2,21 @@ use std::{ env, path::Path, fs::File, io::Write };
 
 const CPU_FREQUENCY: Option<&str> = option_env!("AVR_CPU_FREQUENCY_HZ");
 
-
-
 fn main() {
     let out_dir = env::var("OUT_DIR").expect("Environment variable OUT_DIR not defined");
     let dest_path = Path::new(&out_dir).join("constants.rs");
-    let mut f = File::create(&dest_path).expect("Could not create file");
+    let mut f = File::create(&dest_path).expect("Could not create \"constants.rs\"");
 
     let frequency: u64 = match CPU_FREQUENCY {
         Some(frequency) => match frequency.parse() {
             Ok(freq) => freq,
             Err(_) => {
-                println!("Unable to parse AVR_CPU_FREQUENCY_HZ into u64, defaulting to 16MHz");
+                println!("cargo:warning=Unable to parse AVR_CPU_FREQUENCY_HZ into u64, defaulting to 16MHz");
                 16_000_000
             }
         },
         None => {
-            println!("AVR_CPU_FREQUENCY_HZ not found, defaulting to 16MHz");
+            println!("cargo:warning=AVR_CPU_FREQUENCY_HZ not found, defaulting to 16MHz");
             16_000_000
         },
     };
