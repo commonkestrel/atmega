d156b7e1f4b15124ba00133714bbb08a6c787009
Jibble330 <jettb6@gmail.com>
2023-02-05 22:45:46 -0800

    Improve readability

--- src/atmega/src/prelude.rs
+++ src/atmega/src/prelude.rs
@@ -1,3 +1,8 @@
-pub use crate::{ run, print, println, serial::Serial, timer::{ delay, delay_micros,  }, pins::{ Pin, PinMode, HIGH, LOW, pin_mode, digital_read, digital_write, digital_toggle }, registers::Register };
+pub use crate::serial::Serial;
+pub use crate::registers::Register;
+pub use crate::{ run, print, println };
+pub use crate::timer::{ delay, delay_micros };
+pub use crate::wiring::{ Pin, PinMode, HIGH, LOW, pin_mode, digital_read, digital_write, digital_toggle, analog_read, analog_write };
+
 #[cfg(feature = "millis")]
 pub use crate::timer::millis;
