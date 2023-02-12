a7ff8385567e8a585cd3012c86af941ffe86f1d3
Kestrel <jettb6@gmail.com>
2023-02-11 19:11:07 -0800

    Link to util delay

--- src/time.rs
+++ src/time.rs
@@ -1,73 +1,26 @@
 //! Utilities for reading and controlling time
 
 use crate::constants::CPU_FREQUENCY;
+use crate::registers::{ Register, TCNT1L, TCNT1H, TIFR0, TIMSK0, TCNT0 };
+use crate::util::delay::{ _delay_cycles, _delay_us, _delay_ms, MILLIS, MICROS };
+
 #[cfg(feature = "millis")]
 use crate::volatile::Volatile;
-use crate::registers::{ Register, TCNT1L, TCNT1H, TIFR0, TIMSK0, TCNT0 };
 
-const MICROS: u64 = 100000;
-const MILLIS: u64 = 1000;
-
-/// Reads the current value of Timer/Counter1
-pub fn read_timer1() -> u16 {        
-    // In order to read 16 bit registers on the ATmega328p
-    // you need to read the low byte before the high byte
-    let low_byte =  unsafe { TCNT1L::read() };
-    let high_byte = unsafe { TCNT1H::read() };
-
-    ((high_byte as u16) << 8) | low_byte as u16 // Use both bytes to construct a u16
-}
-
-/// Sleep for the specified number of clock cycles.
-/// Has a precision of 64 cycles.
+/// Sleep for a given number of CPU cycles
+/// Has a precision of 4 cycles
 pub fn delay_cycles(cycles: u64) {
-    // Timer 0 prescaler is set to 64, which means the timer increments every 8 clock cycles
-    let scaled_cycles = cycles/64;
-    
-    let individual = (scaled_cycles%256) as u8;
-    let initial = unsafe{ TCNT0::read() };
-
-    // Checks if (the value already in Timer 0 + the individual ticks needed to delay) are greater than the max value of Timer 0 (2^8)
-    // The value in Timer 0 cannot be changed as this will offset millis()
-    let (of_required, remaining) = if initial.checked_add(individual).is_none() {
-        ((scaled_cycles/256)+1, individual-initial)
-    } else {
-        (scaled_cycles/256, individual)
-    };
-
-    // Disable Timer 0 OVF interrupt
-    // This prevents TOV0 from being cleared automatically
-    unsafe { TIMSK0::TOIE0.clear(); }
-    
-    // The TCNT1 counter is an 8 bit register, so we need to wait for overflow interrupts if the number of cycles is more than 256 (2^8)
-    for _ in 0..of_required {
-        unsafe { 
-            while !TIFR0::TOV0.read_bit() {}
-            // To clear a bit in the TIFR you must write it high
-            TIFR0::write(0b0000_0001);
-        }
-        // Update SYSTICK since the Timer 0 overflow interrupt is captured by this loop
-        #[cfg(feature = "millis")]
-        SYSTICK.operate(|val| val + 1);
-    }
-
-    // Renable Timer 0 OVF interrupt
-    // This allows systick to update
-    unsafe { TIMSK0::TOIE0.set(); }
-
-    // Wait for the remaining cycles
-    unsafe { TCNT0::until(|val| val <= remaining) }
+    _delay_cycles(cycles);
 }
 
 /// Sleep for a given number of microseconds.
-/// Has a precision of 8μs.
 pub fn delay_micros(us: u64) {
-    delay_cycles(us*CPU_FREQUENCY/MICROS);
+    _delay_us(us);
 }
 
 /// Sleep for a given number of milliseconds.
 pub fn delay(ms: u64) {
-    delay_cycles(ms*CPU_FREQUENCY/MILLIS);
+    _delay_ms(ms);
 }
 
 #[cfg(feature = "millis")]
