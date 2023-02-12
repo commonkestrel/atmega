db6cf80afd85d3d88039ed4e7bd135c03c153560
Kestrel <jettb6@gmail.com>
2023-02-12 15:41:24 -0800

    Move delay into time

--- src/time.rs
+++ src/time.rs
@@ -1,26 +1,64 @@
 //! Utilities for reading and controlling time
 
+use core::arch::asm;
 use crate::constants::CPU_FREQUENCY;
 use crate::registers::{ Register, TCNT0 };
-use crate::util::delay::{ _delay_cycles, _delay_us, _delay_ms, MILLIS, MICROS };
 
 #[cfg(feature = "millis")]
 use crate::volatile::Volatile;
 
-/// Sleep for a given number of CPU cycles
-/// Has a precision of 4 cycles
+/// Milliseconds in a second
+pub const MILLIS: u64 = 1_000;
+/// Microseconds in a second
+pub const MICROS: u64 = 1_000_000;
+
+/// Delay loop using a 16 bit counter, so upto 65536 iterations are possible.
+/// (The value 65536 would have to passed as 0)
+/// The loop executes four CPU cycles per iteration,
+/// not including the overhead the compiler requires to setup the counter register pair.
+/// 
+/// Thus, at a CPU speed of 1MHZ, delays of up to about 262.1 
+/// milliseconds can be achieved
+#[inline(always)]
+pub fn _delay_loop(count: u16) {
+    unsafe {
+        asm!(
+            "1: sbiw {0}, 1",
+            "brne 1b",
+            inout(reg_iw) count => _,
+        );
+    }
+}
+
+/// Delay the specified CPU cycles using _delay_loop()
+/// Has a precision of 4 cycles.
+#[inline(always)]
 pub fn delay_cycles(cycles: u64) {
-    _delay_cycles(cycles);
+    // Each iteration in _delay_loop() takes 4 clock cycles
+    let loops = cycles/4;
+
+    let outer = loops / 65536;
+    let last = (loops % 65536 + 1) as u16;
+
+    for _ in 0..outer {
+        // The value 65536 has to be passed to _delay_loop() as 0
+        _delay_loop(0);
+    }
+
+    _delay_loop(last);
 }
 
-/// Sleep for a given number of microseconds.
+/// Delay the specified number of microseconds
+/// On boards with a clock speed of less than 4MHz the precision will be less than 1us
+#[inline(always)]
 pub fn delay_micros(us: u64) {
-    _delay_us(us);
+    delay_cycles(us * (CPU_FREQUENCY/MICROS));
 }
 
-/// Sleep for a given number of milliseconds.
+/// Delay the specified number of milliseconds
+#[inline(always)]
 pub fn delay(ms: u64) {
-    _delay_ms(ms);
+    delay_cycles(ms * (CPU_FREQUENCY/MILLIS));
 }
 
 #[cfg(feature = "millis")]
--- src/util/mod.rs
+++
@@ -1,4 +0,0 @@
-//! Low level utilities for the ATmega
-//! Many higher level functions wrap this.
-
-pub mod delay;
