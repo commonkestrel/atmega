4ff9fe15cd2dbb479a11012f4880e80146826c16
Kestrel <jettb6@gmail.com>
2023-03-01 23:25:22 -0800

    Remove color

--- src/libraries/mod.rs
+++ src/libraries/mod.rs
@@ -1,7 +1,6 @@
 //! Libraries to match the official Arduino language such as Wire and TimeLib
 
 pub mod time;
-pub mod color;
 
 #[cfg(any(feature = "twowire", doc))]
 #[doc(cfg(feature = "twowire"))]
