40952a98c3651f74595fe9988adf689252af7291
Kestrel <jettb6@gmail.com>
2023-03-06 08:12:39 -0800

    Fix syntax errors

--- src/alloc.rs
+++ src/alloc.rs
@@ -3,8 +3,7 @@ extern crate alloc;
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
 
-const MALLOC_MARGIN = 32;
-
+const MALLOC_MARGIN: usize = 32;
 
 struct __freelist {
     sz: usize,
@@ -26,12 +25,13 @@ impl GlobalAlloc for Allocator {
         null_mut()
     }
 
-    unsafe dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
         panic!("dealloc should not be called")
     }
 }
 
-unsafe impl Sync for Allocator;
+unsafe impl Send for Allocator {}
+unsafe impl Sync for Allocator {}
 
 #[global_allocator]
 static ALLOCATOR: Allocator = Allocator;
