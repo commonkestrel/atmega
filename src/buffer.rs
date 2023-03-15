84731c98b69bd6d0de9e22c84d639b148aa3a5bd
Kestrel <jettb6@gmail.com>
2023-03-14 22:58:48 -0700

    Translate to generics

--- src/buffer.rs
+++ src/buffer.rs
@@ -2,25 +2,28 @@
 //! 
 //! This is an adaptation of the `USART_BUFFER` from [`avr_328p_usart`](https://github.com/johncobb/avr_328p_usart)
 
-/// Byte buffer of variable length
+use core::mem::MaybeUninit;
+
+/// Byte buffer of variable length.
+/// Default length is 32.
 #[derive(Debug, Clone, Copy)]
-pub struct Buffer<const SIZE: usize> {
+pub struct Buffer<T: Copy, const SIZE: usize = 32> {
     head: usize,
     tail: usize,
-    buffer: [u8; SIZE],
+    buffer: [MaybeUninit<T>; SIZE],
 }
 
-impl<const SIZE: usize> Buffer<SIZE> {
+impl<T: Copy, const SIZE: usize> Buffer<T, SIZE> {
     /// Maximum size of the buffer
     pub const MAX_SIZE: usize = SIZE;
 
     /// Creates a new buffer set to all 0s
     #[inline(always)]
-    pub const fn new() -> Buffer<SIZE> {
+    pub const fn new() -> Self {
         Buffer {
             head: 0,
             tail: 0,
-            buffer: [0; SIZE],
+            buffer: MaybeUninit::uninit_array(),
         }
     }
 
@@ -28,13 +31,13 @@ impl<const SIZE: usize> Buffer<SIZE> {
     /// 
     /// # Panics
     /// Will panic if the length of the slice is larger than the maximum size of the buffer.
-    pub fn from_slice(data: &[u8]) -> Buffer<SIZE> {
+    pub fn copy_from_slice(data: &[T]) -> Self {
         if data.len() > SIZE {
             panic!("Slice larger than Buffer MAX_SIZE");
         }
         let mut new = Self::new();
         for byte in data {
-            new.write(byte.clone());
+            new.write(*byte);
         }
 
         new
@@ -42,7 +45,7 @@ impl<const SIZE: usize> Buffer<SIZE> {
 
     /// Writes a byte to the head of the buffer.
     /// Does not do anything if the buffer is full.
-    pub fn write(&mut self, value: u8) {
+    pub fn write(&mut self, value: T) {
         let i = (self.head + 1) % SIZE;
 
         // if we should be storing the received character into the location
@@ -50,7 +53,7 @@ impl<const SIZE: usize> Buffer<SIZE> {
         // current location of the tail), we're about to overflow the buffer
         // and so we don't write the character or advance the head.
         if i != self.tail {
-            self.buffer[self.head] = value;
+            self.buffer[self.head].write(value);
             self.head = i;
         }
     }
@@ -60,15 +63,21 @@ impl<const SIZE: usize> Buffer<SIZE> {
         (SIZE + self.head - self.tail) % SIZE
     }
 
+    /// Returns the available space left in the buffer
+    /// before writes are ignored.
+    pub fn available(&self) -> usize {
+        SIZE-1 - self.len()
+    }
+
     /// Reads the byte at the front of the buffer.
     /// Returns `None` if there is no data stored in the buffer.
-    pub fn read(&mut self) -> Option<u8> {
+    pub fn read(&mut self) -> Option<T> {
         // if the head isn't ahead of the tail, we don't have any characters
          if self.head == self.tail {
             return None;
          }
 
-         let value = self.buffer[self.tail];
+         let value = unsafe { self.buffer[self.tail].assume_init() };
          self.tail = (self.tail + 1) % SIZE;
          Some(value)
     }
@@ -92,20 +101,30 @@ impl<const SIZE: usize> Buffer<SIZE> {
     }
 }
 
-impl<const SIZE: usize> core::ops::Index<usize> for Buffer<SIZE> {
-    type Output = u8;
+impl<T: Copy, const SIZE: usize> core::ops::Index<usize> for Buffer<T, SIZE> {
+    type Output = T;
 
     fn index(&self, index: usize) -> &Self::Output {
         if index >= self.len() {
             panic!("Index out of range! Index of {} into Buffer of length {}.", index, self.len());
         }
         let i = self.tail.wrapping_add(index) % SIZE;
-        &self.buffer[i]
+        unsafe { self.buffer[i].assume_init_ref() }
     }
 }
 
-impl<const SIZE: usize> Iterator for Buffer<SIZE> {
-    type Item = u8;
+impl<T: Copy, const SIZE: usize> core::ops::IndexMut<usize> for Buffer<T, SIZE> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        if index >= self.len() {
+            panic!("Index out of range! Index of {} into Buffer of length {}.", index, self.len());
+        }
+        let i = self.tail.wrapping_add(index) % SIZE;
+        unsafe { self.buffer[i].assume_init_mut() }
+    }
+}
+
+impl<T: Copy, const SIZE: usize> Iterator for Buffer<T, SIZE> {
+    type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.read()
